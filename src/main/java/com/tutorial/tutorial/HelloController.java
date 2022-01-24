00cd0bd221d733c2316cade28949d7aad4dcaff9
= <=>
2022-01-24 20:29:15 +0000

    First

--- src/main/java/com/tutorial/tutorial/HelloController.java
+++ src/main/java/com/tutorial/tutorial/HelloController.java
@@ -8,17 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 
 	@GetMapping("/page")
-=======
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HelloController {
-
-	@RequestMapping("/page")
-    @ResponseBody
->>>>>>> 542191ebf8d39f4a1798dd6ba9bc36c8a8b61375
 	public String page() {
 		return "page";
 	}
